8b64bf416aaf801516a371f83456d98a6179c298
candroid <cookiemonster669966@gmail.com>
2019-04-27 01:00:10 -0500

    remove context param from method in its' current scope

--- bootlaces/src/main/java/com/candroid/bootlaces/BootService.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/BootService.kt
@@ -24,7 +24,7 @@ abstract class BootService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        startForeground(666, createNotification(this))
+        startForeground(666, createNotification())
         return START_STICKY;
     }
 
@@ -37,24 +37,24 @@ abstract class BootService : Service() {
         isRunning = false
     }
 
-    private fun createNotification(context: Context): Notification {
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    private fun createNotification(): Notification {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(getString(R.string.channel_id), getString(R.string.channel_id), NotificationManager.IMPORTANCE_HIGH)
             notificationManager.createNotificationChannel(notificationChannel)
         }
-        val builder = NotificationCompat.Builder(context)
-        with(PreferenceManager.getDefaultSharedPreferences(context)) {
+        val builder = NotificationCompat.Builder(this)
+        with(PreferenceManager.getDefaultSharedPreferences(this)) {
             var icon = getInt(KEY_NOTIFICATION_ICON, -1)
             if (icon == -1) icon = android.R.drawable.sym_def_app_icon
             builder.setContentTitle(getString(KEY_NOTIFICATION_TITLE, "candroidtb"))
                 .setContentText(getString(KEY_NOTIFICATION_CONTENT, "boot laces"))
                 .setSmallIcon(icon)
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) getString(KEY_CLICKED_ACTIVITY_NAME, null)?.let {
-                val intent = Intent(context, Class.forName(it))
+                val intent = Intent(this@BootService, Class.forName(it))
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                 intent.action = Intent.ACTION_VIEW
-                val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+                val pendingIntent = PendingIntent.getActivity(this@BootService, 0, intent, 0)
                 builder.setContentIntent(pendingIntent)
             }
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
