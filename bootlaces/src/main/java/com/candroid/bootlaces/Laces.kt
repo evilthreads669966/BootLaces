32b3fa8e8e209349cae4bbc813809b3ccf660a53
candroid <cookiemonster669966@gmail.com>
2019-04-26 21:21:50 -0500

    private static persist service method

--- bootlaces/src/main/java/com/candroid/bootlaces/Laces.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/Laces.kt
@@ -8,17 +8,21 @@ import android.preference.PreferenceManager
 class Laces{
     companion object{
         fun tie(context: Context, serviceName: String, notificationTitle: String = "candroid", notificationContent: String = "boot laces", notificationIcon: Int = -1, notificationClickActivity: Class<Any>? = null){
+            persistService(context, serviceName, notificationTitle, notificationContent, notificationIcon, notificationClickActivity)
+            startService(context, serviceName)
+        }
+
+        private fun persistService(context: Context, serviceName: String, notificationTitle: String, notificationContent: String, notificationIcon: Int, notificationClickActivity: Class<Any>?){
             with(PreferenceManager.getDefaultSharedPreferences(context)){
                 val serviceClassName = getString(BootReceiver.KEY_SERVICE_CLASS_NAME, "null")
                 if(serviceClassName.equals("null")) edit()?.apply {
-                        putString(BootReceiver.KEY_SERVICE_CLASS_NAME, serviceName)
-                        putString(BootService.KEY_NOTIFICATION_TITLE, notificationTitle)
-                        putString(BootService.KEY_NOTIFICATION_CONTENT, notificationContent)
-                        putInt(BootService.KEY_NOTIFICATION_ICON, notificationIcon)
-                        putString(BootService.KEY_CLICKED_ACTIVITY_NAME, notificationClickActivity?.name)
-                    }?.apply()
+                    putString(BootReceiver.KEY_SERVICE_CLASS_NAME, serviceName)
+                    putString(BootService.KEY_NOTIFICATION_TITLE, notificationTitle)
+                    putString(BootService.KEY_NOTIFICATION_CONTENT, notificationContent)
+                    putInt(BootService.KEY_NOTIFICATION_ICON, notificationIcon)
+                    putString(BootService.KEY_CLICKED_ACTIVITY_NAME, notificationClickActivity?.name)
+                }?.apply()
             }
-            startService(context, serviceName)
         }
 
         private fun startService(context: Context, serviceName: String){
