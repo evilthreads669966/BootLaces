29f42c5711435857b3a146e2757eac524bdbc067
candroid <cookiemonster669966@gmail.com>
2019-05-03 02:41:14 -0500

    kdoc BootPreferences edit again again

--- bootlaces/src/main/java/com/candroid/bootlaces/BootPreferences.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/BootPreferences.kt
@@ -24,7 +24,7 @@ import android.util.Log
 
 /**
  * BootPreferences provides [getInstance] which returns the [SharedPreferences] file located in device protected storage
- * used for when Direct Boot is activated and the application needs to write/access data to [SharedPreferences] before the user enters
+ * used for when Direct Boot is activated and the application needs to read/write from/to [SharedPreferences] before the user enters
  * their device credentials
  *
  */
