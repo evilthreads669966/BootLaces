e572a3d5235ca1248fc9500c7c470097f93dc27b
evilthreads <evilthreads669966@gmail.com>
2020-10-30 15:19:26 -0500

    evil threads!

--- bootlaces/src/main/java/com/candroid/bootlaces/BackgroundWorkService.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/BackgroundWorkService.kt
@@ -1,3 +1,16 @@
+/*Copyright 2019 Chris Basinger
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 package com.candroid.bootlaces
 
 import android.content.BroadcastReceiver
@@ -17,7 +30,26 @@ import kotlinx.coroutines.flow.collect
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Provider
-
+/*
+            (   (                ) (             (     (
+            )\ ))\ )    *   ) ( /( )\ )     (    )\ )  )\ )
+ (   (   ( (()/(()/(  ` )  /( )\()|()/((    )\  (()/( (()/(
+ )\  )\  )\ /(_))(_))  ( )(_)|(_)\ /(_))\((((_)( /(_)) /(_))
+((_)((_)((_|_))(_))   (_(_()) _((_|_))((_))\ _ )(_))_ (_))
+| __\ \ / /|_ _| |    |_   _|| || | _ \ __(_)_\(_)   \/ __|
+| _| \ V /  | || |__    | |  | __ |   / _| / _ \ | |) \__ \
+|___| \_/  |___|____|   |_|  |_||_|_|_\___/_/ \_\|___/|___/
+....................../´¯/)
+....................,/¯../
+.................../..../
+............./´¯/'...'/´¯¯`·¸
+........../'/.../..../......./¨¯\
+........('(...´...´.... ¯~/'...')
+.........\.................'...../
+..........''...\.......... _.·´
+............\..............(
+..............\.............\...
+*/
 /**
  * @author Chris Basinger
  * @email evilthreads669966@gmail.com
@@ -69,6 +101,7 @@ abstract class BackgroundWorkService: LifecycleService() {
         stopSelfResult(startId)
         super.onDestroy()
     }
+
     suspend fun handleWorkers(){
         foreground.scope.launch {
             ticker(5000,3000).consumeEach {
