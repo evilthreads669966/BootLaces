21337bb3b5fca76796f0381cfb224126e304b36e
Chris Basinger <evilthreads669966@gmail.com>
2021-01-23 07:25:32 -0600

    Scheduler Scoping Function and KTX Worker Schedule Functions (#155)
    - scheduler provides a use scoping function
      - receiver type WorkScheduler
      - use this scope to schedule your workers
        - all Scheduler.schedule<TIME> functions are now Worker.schedule<TIME> functions
          - receiver type Worker
        - you must be inside Scheduler.use scope in order to schedule
      - Scheduler.sendWorkAlarm & Scheduler.sendWorkPersistent are both KTX functions
        - receiver type Worker
        - both renamed to Worker.schedule
          - overloaded arguments to distinguish between the two
            - internal api only 
    - rename all workers to WorkerOne & WorkerTwo all the way until 13
    - delete demo app code and only demonstrate how to properly schedule workers with the new API

--- bootlaces/src/main/java/com/candroid/bootlaces/IntentFactory.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/IntentFactory.kt
@@ -56,19 +56,20 @@ import javax.inject.Singleton
 @InternalCoroutinesApi
 @Singleton
 class IntentFactory @Inject constructor(@ApplicationContext val ctx: Context){
-    internal fun createWorkNotificationIntent(worker: Worker) = Intent().apply {
+
+    internal fun createWorkNotificationIntent(worker: Worker): Intent = Intent().apply {
         setAction(Actions.ACTION_START.action)
         putExtra(NotificatonService.KEY_ID, worker.id)
         putExtra(NotificatonService.KEY_DESCRIPTION, worker.description)
     }
 
-    internal fun createWorkIntent(work: Work, actions: Actions) = Intent().apply {
+    internal fun createWorkIntent(work: Work, actions: Actions): Intent = Intent().apply {
         setClass(ctx, WorkService::class.java)
         setAction(actions.action)
         putExtra(Work.KEY_PARCEL, work)
     }
 
-    private fun createAlarmIntent(work: Work) = createWorkIntent(work, Actions.ACTION_WORK_NOW).run {
+    private fun createAlarmIntent(work: Work): Intent? = createWorkIntent(work, Actions.ACTION_WORK_NOW).run {
         when{
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
                 PendingIntent.getForegroundService(ctx, work.id, this, PendingIntent.FLAG_NO_CREATE)?.let { null } ?: this
