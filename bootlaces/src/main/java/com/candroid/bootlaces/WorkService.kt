9d881a3cab759b63a1b4c11161f075fca362f892
Chris Basinger <evilthreads669966@gmail.com>
2021-01-23 19:15:58 -0600

    Last Mistake With startId (#167)
    - stopSelf if startId equals -1

--- bootlaces/src/main/java/com/candroid/bootlaces/WorkService.kt
+++ bootlaces/src/main/java/com/candroid/bootlaces/WorkService.kt
@@ -80,7 +80,7 @@ class WorkService: Service(), ComponentCallbacks2,IWorkHandler<Worker,CoroutineS
         if(newValue == 0){
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
                 foreground.deactivate()
-            if(startId != -1) stopSelf()
+            if(startId == -1) stopSelf()
             else if(!stopSelfResult(startId))
                 stopSelf()
         }
